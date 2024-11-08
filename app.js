a99d1ae9ac97bf73e99b7562b5e8d9adccd31c12
Sathishkumar T <29483566+Sathish2905@users.noreply.github.com>
2024-11-09 01:13:58 +0530

    port

--- app.js
+++ app.js
@@ -51,5 +51,5 @@ const PORT = process.env.PORT || 3000;
 
 sequelize.sync().then(() => {
     console.log('Database connected');
-    app.listen(PORT, () => console.log('Server is running on port ${PORT}'));
+    app.listen(PORT, () => console.log('Server is running on port '+ PORT));
 }).catch(err => console.error('Database connection error:', err));
